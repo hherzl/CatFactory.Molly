af98c7b65b787ae06446a4a37819d69604eb26a0
HH <hans.herzl@live.com>
2019-02-11 00:16:15 -0600

    Code refactor: addition of ASP.NET Core 2.2 project

---
+++ Backend/CatFactory.UI.WebAPI/CatFactory.UI.WebAPI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace CatFactory.UI.WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost
+                .CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:8400")
+                .UseStartup<Startup>();
+    }
+}

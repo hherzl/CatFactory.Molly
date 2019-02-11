ea807a80e3bcf055c596dce786f30de449e648e8
HH <hans.herzl@live.com>
2019-02-11 00:37:07 -0600

    Code refactor: upgrade Web API project to ASP.NET Core 2.2

---
+++ Backend/CatFactory.UI.WebAPI/CatFactory.UI.WebAPI/Models/Tokens.cs
@@ -0,0 +1,10 @@
+﻿namespace CatFactory.UI.WebAPI.Models
+{
+    public static class Tokens
+    {
+        public const string Schema = "schema";
+        public const string Table = "table";
+        public const string View = "view";
+        public const string MsDescription = "MS_Description";
+    }
+}

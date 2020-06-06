f6122af39478546ac72b5923ef5d2985ad75901e
HH <hans.herzl@live.com>
2020-06-06 10:28:01 -0600

    Addition of Home component

--- Frontend/cat-factory-ui/src/app/app.module.ts
+++ Frontend/cat-factory-ui/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,

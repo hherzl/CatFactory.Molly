cb04187d0e5b57ae7ce4f3f42bf28d43a18a4d61
HH <hans.herzl@live.com>
2020-06-12 17:59:44 -0600

    Fix #21: Addition of log using Serilog package

--- src/backend/CatFactory.UI/CatFactory.UI.API/Program.cs
+++ src/backend/CatFactory.UI/CatFactory.UI.API/Program.cs
@@ -1,17 +1,44 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace CatFactory.UI.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich
+                .FromLogContext()
+                    .WriteTo.Console()
+                    .WriteTo.File(@"C:\Logs\CatFactory.UI\CatFactory.UI.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder

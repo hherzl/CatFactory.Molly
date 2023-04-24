0d903fbc40f78060a06a7eba183bcc4889a80f09
HH <hans.herzl@live.com>
2023-04-24 01:14:01 -0600

    Installation of CatFactory.SqlServer package

---
+++ Source/Backend/CatFactory.GUI/Source/CatFactory.GUI.API/Controllers/CodeFactoryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatFactory.GUI.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1")]
+    public class CodeFactoryController : ControllerBase
+    {
+        private readonly ILogger<CodeFactoryController> _logger;
+
+        public CodeFactoryController(ILogger<CodeFactoryController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}

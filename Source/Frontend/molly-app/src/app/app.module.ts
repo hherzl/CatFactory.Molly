17ec7364dad152e396d134de75b1ed11e8c49797
HH <hans.herzl@live.com>
2023-04-26 01:08:36 -0600

    Addition of home component

--- Source/Frontend/molly-app/src/app/app.module.ts
+++ Source/Frontend/molly-app/src/app/app.module.ts
@@ -11,11 +11,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavmenuComponent
+    NavmenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
